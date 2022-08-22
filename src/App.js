e06c64e0449c47a84b50632e69e54f47065f412a
paramkeshkamat <keshkamatparam@gmail.com>
2022-08-22 17:12:52 +0530

    password generate logic added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import PG from './PG';
-import UGCstaffadd from './components/UGCstaffadd'
+import React from "react";
+import PG from "./PG";
+import UGCstaffadd from "./components/UGCstaffadd";
 
 export const App = () => {
   return (
- <div className="container">
-  <div className="row">
-    <div className="col-md-6">
-        <UGCstaffadd />
-     
+    <div className="container" style={{ marginTop: 100 }}>
+      <div className="row">
+        <div className="col-md-6">
+          <UGCstaffadd />
+        </div>
+        {/* <div className="col-md-6">
+          <PG />
+        </div> */}
+      </div>
     </div>
-    <div className="col-md-6">
-       <PG />
-    </div>
-  </div>
-</div>
-  )
-}
+  );
+};
 export default App;
